ab3046f9cb70bb7517a58089af7947f0e44e2203
Tilman Holube <tilman@holube.de>
2025-10-20 13:55:09 +0200

    add annotation processor to module info

--- annotation-processor/src/main/java/module-info.java
+++ annotation-processor/src/main/java/module-info.java
@@ -1,4 +1,10 @@
+import org.schlunzis.zis.stomp.processor.StompClientPublisherProcessor;
+
+import javax.annotation.processing.Processor;
+
 module org.schlunzis.stomp.client.annotation.processor {
     requires org.schlunzis.zis.stomp.client;
     requires java.compiler;
+
+    provides Processor with StompClientPublisherProcessor;
 }
