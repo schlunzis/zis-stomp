d670f7acfe96443d0968f2ae1ac21dc17cdae617
Tilman Holube <tilman@holube.de>
2025-10-23 19:07:27 +0200

    Integration tests for Jackson ObjectMapper service loading

---
+++ client/src/it/tools-jackson/src/main/java/org/schlunzis/zis/stomp/client/it/Main.java
@@ -0,0 +1,34 @@
+package org.schlunzis.zis.stomp.client.it;
+
+import org.schlunzis.zis.stomp.client.StompClient;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) throws URISyntaxException, InterruptedException {
+        StompClient stompClient = StompClient.builder()
+                .endpoint(new URI("ws://localhost:8080/ws"))
+                .build();
+
+        stompClient.connect();
+        CountDownLatch latch = new CountDownLatch(1);
+        Model model = new Model(UUID.randomUUID(), "Test");
+
+        stompClient.subscribe("/insight/simple/echo", Model.class, m -> {
+            if (m.equals(model))
+                latch.countDown();
+        });
+        stompClient.send("/server/simple/echo", model);
+
+        if (!latch.await(10, TimeUnit.SECONDS))
+            System.exit(1);
+
+        stompClient.close();
+    }
+
+}
