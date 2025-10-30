61275003ebc499c3c88b052641144c8ac2612c8c
Tilman Holube <tilman@holube.de>
2025-10-30 22:55:25 +0100

    add integration tests for annotation processor (#50)
    * add integration tests for annotation processor
    
    * fix string echo

--- test/mock-server/src/main/java/org/schlunzis/zis/stomp/mock_server/SimpleController.java
+++ test/mock-server/src/main/java/org/schlunzis/zis/stomp/mock_server/SimpleController.java
@@ -26,4 +26,13 @@ public class SimpleController {
         this.template.convertAndSend("/insight/simple/echo", message);
     }
 
+    @MessageMapping("/echo/string")
+    public void echo(String message) {
+        if (message == null || message.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.template.convertAndSend("/insight/simple/echo", message);
+    }
+
 }
